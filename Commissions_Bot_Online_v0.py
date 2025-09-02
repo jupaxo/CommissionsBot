707cb7fff486e663546f40cd8b7661eabb732238
jupaxo <juanp.sclavijo@gmail.com>
2025-09-02 09:02:43 -0500

    Update Commissions_Bot_Online_v0.py

--- Commissions_Bot_Online_v0.py
+++ Commissions_Bot_Online_v0.py
@@ -95,7 +95,7 @@ Opportunity Type: New Business, Subscription Months: 36, % of ACV eligible for q
 Opportunity Type: New Business, Subscription Months: 37 - 48, % of ACV eligible for quota credit: 110%
 Opportunity Type: New Business, Subscription Months: greater than 48, % of ACV eligible for quota credit: 120%
 Opportunity Type: Add-On, Subscription Months: Any (must be > 6 months & co-terminous), % of ACV eligible for quota credit: 100%
-Ingrid works in Coupa and lives in Ireland
+if someone ask you about Ingrid, she works in Coupa and lives in Ireland
 """
 # --- SIDEBAR ---
 # Add your logo and a title to the sidebar
@@ -158,6 +158,7 @@ Tus usuarios son los vendedores de la empresa. Utiliza un lenguaje claro, profes
                 st.markdown(ai_answer)
     else:
         st.warning("Please enter a question.")
+
 
 
 
